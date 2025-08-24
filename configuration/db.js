d63e7c6a35f9d7ef10d155c20fbd13d2c0d838a0
Amal Paul <iamamalpaul1367@gmail.com>
2025-08-24 12:00:34 +0530

    change mongo uri

--- configuration/db.js
+++ configuration/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDB = async () => {
   try {
-    const mongoURI = 'mongodb://127.0.0.1:27017/sportshub';
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sportshub';
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
