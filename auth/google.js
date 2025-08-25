bd26098c96247204a6704e86924130a40b8f1ac4
Amal Paul <iamamalpaul1367@gmail.com>
2025-08-25 09:19:33 +0530

    googleauth callback changed

--- auth/google.js
+++ auth/google.js
@@ -5,7 +5,7 @@ const usersSchema = require('../models/usersSchema')
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'https://sports-hub.shop/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
   async (accessToken, refreshToken, profile, done) => {
     try {
