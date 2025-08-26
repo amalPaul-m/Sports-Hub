d41925f3a08059847e2125cb42c2aff279b72cf2
Amal Paul <iamamalpaul1367@gmail.com>
2025-08-26 12:06:28 +0530

    fix forgot password

--- routes/reset.js
+++ routes/reset.js
@@ -5,8 +5,8 @@ const userAuthantication = require('../middleware/userAuthantication');
 const restControllers = require('../controllers/rest')
 
 
-router.get('/',userAuthantication, checkBlockedUser, restControllers.getRest);
+router.get('/',restControllers.getRest);
 
-router.post('/',userAuthantication, checkBlockedUser, restControllers.postRest);
+router.post('/',restControllers.postRest);
 
 module.exports = router;
