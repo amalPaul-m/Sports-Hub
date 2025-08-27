b37d8a79350266ad44800ea5daf511361c1d8509
Amal Paul <iamamalpaul1367@gmail.com>
2025-08-27 12:07:16 +0530

    test per amount

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -471,7 +471,7 @@ const postWallet = async (req, res, next) => {
 
     let discountPer = couponData ? couponData.discountPercentage : 0
     
-    if(discount >= 4900) {
+    if(discount >= 4500) {
         discountPer = Math.floor((5000/totalAmount)*100);
         discount = Math.floor((discountPer/100)*totalAmount);
     }
