09a89706d2adc641682d9bb2440c15296b43e14a
Amal Paul <iamamalpaul1367@gmail.com>
2025-09-01 12:47:45 +0530

    add constants http status code

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,9 @@
+const HttpStatus = require('../constants/httpStatus');
 
 module.exports = (err, req, res, next) => {
   console.error(`[ERROR] ${err.message}`);
 
-  res.status(res.statusCode !== 200 ? res.statusCode : 500).json({
+  res.status(res.statusCode !== HttpStatus.OK ? res.statusCode : HttpStatus.INTERNAL_SERVER_ERROR).json({
     success: false,
     message: err.message || 'Internal Server Error',
   });
