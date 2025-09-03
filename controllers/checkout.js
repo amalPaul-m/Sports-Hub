625a3bfe9d898a1cc2f21da155c06c8a598735df
Amal Paul <iamamalpaul1367@gmail.com>
2025-09-03 14:48:28 +0530

    hold cart data when complete the payment

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -312,7 +312,8 @@ const getPayment = async (req, res, next) => {
   const user = usersData._id;
 
     const cart = await cartSchema.findOne({ userId: user }).populate('items.productId');
-    if (!cart || !cart?.items?.length) {
+    const stockHoldData = await stockHoldSchema.findOne({userId: user});
+    if (!stockHoldData?.items?.length && !cart && !cart?.items?.length) {
       return res.redirect('/cart?error=empty_cart');
     }
 
